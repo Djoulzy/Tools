573b370a4fec4fe52b1625eb3ff12f2483a33c0e
Julien Marusi <julien.marusi@watchever.com>
2017-09-18 10:46:33 +0200

    Amelioration detection fichier de conf

--- config/config.go
+++ config/config.go
@@ -11,6 +11,15 @@ import (
 	"github.com/go-ini/ini"
 )
 
+func tryingFile(confFile string) error {
+	clog.Output("Trying to load conf file %s ...", confFile)
+	if _, err := os.Stat(confFile); os.IsNotExist(err) {
+		return errors.New("Can't find conf file")
+	} else {
+		return nil
+	}
+}
+
 // Load de conf ini file and initialize the struct
 func Load(iniName string, data interface{}) error {
 	var confFile string
@@ -25,10 +34,12 @@ func Load(iniName string, data interface{}) error {
 		}
 		etcpath := strings.TrimRight(gopath, "/")
 		confFile = fmt.Sprintf("%s/etc/%s", etcpath, iniName)
+		if tryingFile(confFile) != nil {
+			confFile = fmt.Sprintf("/etc/%s", iniName)
+		}
 	}
 
-	clog.Output("Trying to load conf file %s ...", confFile)
-	if _, err := os.Stat(confFile); os.IsNotExist(err) {
+	if tryingFile(confFile) != nil {
 		clog.Output("No conf file found, using default values")
 		return errors.New("Can't find conf file")
 	}
